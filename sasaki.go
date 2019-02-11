49e90bd4153e68f15cf80122dc83dda06e5b545d
arvinddeshraj <arvind.deshraj@gmail.com>
2019-02-11 23:44:19 +0530

    go fmt saves the day. Formatted sasaki properly.

--- sasaki.go
+++ sasaki.go
@@ -3,50 +3,50 @@ package main
 import (
 	"fmt"
 	"math/rand"
-	"time"
 	"sync"
+	"time"
 )
 
 type Block struct {
-	val int
-	ismarked bool	
+	val      int
+	ismarked bool
 }
 
 type Process struct {
 	value Block
-	area int
+	area  int
 }
 
 func init_list(proc []Process, order int) {
 	n := len(proc)
 	for i := 0; i < len(proc); i++ {
-		if order == 1 {	
+		if order == 1 {
 			proc[i].value.val = n - i
 		} else if order == 2 {
-			proc[i].value.val = rand.Intn(12345)	
+			proc[i].value.val = rand.Intn(12345)
 		} else {
-			proc[i].value.val = i  
+			proc[i].value.val = i
 		}
 		proc[i].area = 0
-		if i != 0 && i != len(proc) -1 {
+		if i != 0 && i != len(proc)-1 {
 			proc[i+1].value.val = proc[i].value.val
 			proc[i+1].value.ismarked = false
-			i += 1	
+			i += 1
 		} else {
 			proc[i].value.ismarked = true
 		}
 		if i == 0 {
 			proc[i].area = -1
 		}
-	}	
+	}
 }
 
 func display_current(proc []Process) {
 	for _, v := range proc {
 		if v.value.ismarked == true {
-			fmt.Print(v.value.val,"*\t")
+			fmt.Print(v.value.val, "*\t")
 		} else {
-			fmt.Print(v.value.val,"\t")
+			fmt.Print(v.value.val, "\t")
 		}
 	}
 	fmt.Println()
@@ -63,7 +63,7 @@ func SendAndReceive(proc []Process, i int) {
 			proc[i+1].area += 1
 			proc[i+1].area -= 1
 		}
-		temp = proc[i].value//.value.val
+		temp = proc[i].value //.value.val
 		proc[i].value = proc[i+1].value
 		proc[i+1].value = temp
 	}
@@ -74,54 +74,54 @@ func sortNeighbours(proc []Process, i int) {
 	if proc[i].value.val > proc[i+1].value.val {
 		temp = proc[i].value
 		proc[i].value = proc[i+1].value
-		proc[i+1].value =temp
+		proc[i+1].value = temp
 	}
 }
 func solutionSelection(proc []Process) {
-	fmt.Print(proc[0].value.val,"\t")
-	for i :=1; i < len(proc) - 1; i+=2 {
+	fmt.Print(proc[0].value.val, "\t")
+	for i := 1; i < len(proc)-1; i += 2 {
 		if proc[i].area == -1 {
-				fmt.Print(proc[i+1].value.val, "\t")
+			fmt.Print(proc[i+1].value.val, "\t")
 		} else {
-				fmt.Print(proc[i].value.val, "\t")
-			}
-		
-	} 
-	fmt.Print(proc[len(proc) - 1].value.val,"\t")
+			fmt.Print(proc[i].value.val, "\t")
+		}
+
+	}
+	fmt.Print(proc[len(proc)-1].value.val, "\t")
 	fmt.Println()
 }
 
 func main() {
-	rand.Seed(time.Now().UTC().UnixNano())	
+	rand.Seed(time.Now().UTC().UnixNano())
 	var wg sync.WaitGroup
-	n := rand.Intn(100) + 1;
-	processes := make([]Process, 2*n - 2)
+	n := rand.Intn(100) + 1
+	processes := make([]Process, 2*n-2)
 	init_list(processes, 2)
 	display_current(processes)
-	for i := 0; i < n - 1 ; i++ {
-		for j,_ := range processes {
-			if j % 2 == 1 {
+	for i := 0; i < n-1; i++ {
+		for j, _ := range processes {
+			if j%2 == 1 {
 				continue
 			} else {
 				wg.Add(1)
 				go func(processes []Process, j int) {
 					SendAndReceive(processes, j)
 					wg.Done()
-				}(processes, j)	
-			}
-		}
-		wg.Wait()
-		for j,_ := range processes {
-			if j % 2  == 1 && j != len(processes) - 1 {
-				wg.Add(1)
-				go func(processes []Process, j int) {
-					sortNeighbours(processes, j)
-					wg.Done()					
 				}(processes, j)
 			}
 		}
 		wg.Wait()
-		fmt.Println("The list after Round:", i)	
+		for j, _ := range processes {
+			if j%2 == 1 && j != len(processes)-1 {
+				wg.Add(1)
+				go func(processes []Process, j int) {
+					sortNeighbours(processes, j)
+					wg.Done()
+				}(processes, j)
+			}
+		}
+		wg.Wait()
+		fmt.Println("The list after Round:", i)
 		display_current(processes)
 	}
 	fmt.Println("The final solution is:")
