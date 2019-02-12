918cf18813330373800bfc6e8fd520bc9b9f549a
arvinddeshraj <arvind.deshraj@gmail.com>
2019-02-12 18:41:46 +0530

    Restructured Sasaki Implementation (changed to left-right pairs)

--- sasaki.go
+++ sasaki.go
@@ -17,10 +17,11 @@ type Block struct {
 }
 
 // Process is the main component of the list.
-// Each Process contains a single block as well as
+// Each Process contains a left block, a right block as well as
 // a area variable that is used for solution selection
 type Process struct {
-	value Block
+	leftvalue Block
+	rightvalue Block
 	area  int
 }
 
@@ -32,84 +33,90 @@ type Process struct {
 // Also initialises the area variable to zero (except for first element)
 func InitList(proc []Process, order int) {
 	n := len(proc)
-	for i := 0; i < len(proc); i++ {
+	for i := 0; i < n; i++ {
 		if order == 1 {
-			proc[i].value.val = n - i
+			proc[i].leftvalue.val = n - i
+			proc[i].rightvalue.val = n - i
 		} else if order == 2 {
-			proc[i].value.val = rand.Intn(12345)
+			proc[i].leftvalue.val = rand.Intn(12345)
+			proc[i].rightvalue.val =proc[i].leftvalue.val
 		} else {
-			proc[i].value.val = i
+			proc[i].leftvalue.val = i
+			proc[i].rightvalue.val = i
 		}
 		proc[i].area = 0
-		if i != 0 && i != len(proc)-1 {
-			proc[i+1].value.val = proc[i].value.val
-			proc[i+1].value.ismarked = false
-			i += 1
-		} else {
-			proc[i].value.ismarked = true
-		}
-		if i == 0 {
-			proc[i].area = -1
+		if i == 0 || i == n -1 {
+			proc[i].leftvalue.ismarked = true
+			proc[i].rightvalue.ismarked = true
 		}
 	}
+	proc[0].area = -1
 }
-
 // utility function to display the current state of each process
 func DisplayCurrent(proc []Process) {
 	for _, v := range proc {
-		if v.value.ismarked == true {
-			fmt.Print(v.value.val, "*\t")
+		if v.leftvalue.ismarked == true {
+			fmt.Print(v.leftvalue.val,"*\t")
 		} else {
-			fmt.Print(v.value.val, "\t")
+			fmt.Print(v.leftvalue.val,"\t")
+		}
+		if v.rightvalue.ismarked == true {
+			fmt.Print(v.rightvalue.val,"*\t")
+		} else {
+			fmt.Print(v.rightvalue.val,"\t")
 		}
 	}
 	fmt.Println()
 }
-
 // SendAndReceive is a function used to simulate the send and receive operations
 // that would occur in a distributed system. It also changes the area variables based 
 // on the movement of the marked numbers.
 func SendAndReceive(proc []Process, i int) {
 	var temp Block
-	if proc[i].value.val > proc[i+1].value.val {
-		if proc[i].value.ismarked == true && proc[i+1].value.ismarked == false {
+	if proc[i].rightvalue.val > proc[i+1].leftvalue.val {
+		if proc[i].rightvalue.ismarked == true && proc[i+1].leftvalue.ismarked == false	{
 			proc[i+1].area -= 1
-		} else if proc[i].value.ismarked == false && proc[i+1].value.ismarked == true {
+		} else if proc[i].rightvalue.ismarked == false && proc[i+1].leftvalue.ismarked == true {
 			proc[i+1].area += 1
-		} else if proc[i].value.ismarked == true && proc[i+1].value.ismarked == true {
+		} else if proc[i].rightvalue.ismarked == true && proc[i+1].leftvalue.ismarked == true {
 			proc[i+1].area += 1
 			proc[i+1].area -= 1
 		}
-		temp = proc[i].value 
-		proc[i].value = proc[i+1].value
-		proc[i+1].value = temp
+		temp = proc[i].rightvalue
+		proc[i].rightvalue = proc[i+1].leftvalue
+		proc[i+1].leftvalue = temp
 	}
 }
-
 // SortNeighbours is used to order the values in the middle processes
 // (processes other than the first and last)
 func SortNeighbours(proc []Process, i int) {
 	var temp Block
-	if proc[i].value.val > proc[i+1].value.val {
-		temp = proc[i].value
-		proc[i].value = proc[i+1].value
-		proc[i+1].value = temp
+	if i == 0 {
+		proc[i].leftvalue = proc[i].rightvalue
+	}
+	if i + 1 == len(proc) - 1 {
+		proc[i+1].rightvalue = proc[i+1].leftvalue
+	}
+	if proc[i].leftvalue.val > proc[i].rightvalue.val {
+		temp = proc[i].rightvalue
+		proc[i].rightvalue = proc[i].leftvalue
+		proc[i].leftvalue = temp
 	}
 }
 
 // SolutionSelection is a function to select the left or right value for the final solution
 // after n-1 rounds.
 func SolutionSelection(proc []Process) {
-	fmt.Print(proc[0].value.val, "\t")
-	for i := 1; i < len(proc)-1; i += 2 {
+	fmt.Print(proc[0].rightvalue.val, "\t")
+	for i := 1; i < len(proc)-1; i++ {
 		if proc[i].area == -1 {
-			fmt.Print(proc[i+1].value.val, "\t")
+			fmt.Print(proc[i].rightvalue.val, "\t")
 		} else {
-			fmt.Print(proc[i].value.val, "\t")
+			fmt.Print(proc[i].leftvalue.val, "\t")
 		}
 
 	}
-	fmt.Print(proc[len(proc)-1].value.val, "\t")
+	fmt.Print(proc[len(proc)-1].leftvalue.val, "\t")
 	fmt.Println()
 }
 
@@ -119,14 +126,12 @@ func main() {
 	// It ensures that sorting of intermeditate processes' values
 	// happens only after all goroutines finish their swaps
 	n := rand.Intn(100) + 1
-	processes := make([]Process, 2*n-2)
+	processes := make([]Process, n)
 	InitList(processes, 2)
 	DisplayCurrent(processes)
 	for i := 0; i < n-1; i++ {
 		for j, _ := range processes {
-			if j%2 == 1 {
-				continue
-			} else {
+			if j != n -1 {
 				wg.Add(1)
 				go func(processes []Process, j int) {
 					SendAndReceive(processes, j)
@@ -136,7 +141,7 @@ func main() {
 		}
 		wg.Wait()
 		for j, _ := range processes {
-			if j%2 == 1 && j != len(processes)-1 {
+			if j != len(processes)-1 {
 				wg.Add(1)
 				go func(processes []Process, j int) {
 					SortNeighbours(processes, j)
